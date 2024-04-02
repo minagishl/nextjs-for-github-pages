52f46697fb826d449a1efec30307827cae5204d5
minagishl <mvxd568.co@icloud.com>
2024-04-03 00:23:10 +0900

    Update template

---
+++ src/app/config.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? '変数が指定されていません';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION ?? '';
+export const CREATOR = process.env.NEXT_PUBLIC_CREATOR ?? '@minagishl';
+export const GOOGLE_VERIFICATION = process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION ?? '';
