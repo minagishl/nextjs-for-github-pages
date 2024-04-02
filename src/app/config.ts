24ada2d3e7e4ecac0355653379ddac04a73191bf
minagishl <mvxd568.co@icloud.com>
2024-04-03 00:40:50 +0900

    Fixes related to configuration files

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,9 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? '変数が指定されていません';
-export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION ?? '';
-export const CREATOR = process.env.NEXT_PUBLIC_CREATOR ?? '@minagishl';
-export const GOOGLE_VERIFICATION = process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION ?? '';
+import YAML from 'yaml'
+
+const config_yaml = require('../constants/config.yaml') as string
+const config = YAML.parse(config_yaml)
+
+export const APP_NAME = config.APP_NAME ?? '変数が指定されていません';
+export const APP_DESCRIPTION = config.APP_DESCRIPTION ?? '';
+export const CREATOR = config.CREATOR ?? '@minagishl';
+export const GOOGLE_VERIFICATION = config.GOOGLE_VERIFICATION ?? '';
